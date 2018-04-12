50e2271fb899c5ba402c87d1c3fca1c2f76fad25
Marcus Jun <mjun@calstate.edu>
2018-04-12 20:55:20 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,39 +3,39 @@ class Book
     @title=title
   end
 
-  def title
-    @title
-  end
+  #def title
+    #@title
+  #end
 
   #attr_accessor :author
 
-  def author=(author)
-    @author=author
-  end
+  #def author=(author)
+    #@author=author
+  #end
 
-  def author
-    @author
-  end
+  #def author
+    #@author
+  #end
 
   #attr_accessor :page_count
 
-  def page_count=(num)
-    @page_count=num
-  end
+  #def page_count=(num)
+    #@page_count=num
+  #end
 
-  def page_count
-    @page_count
-  end
+  #def page_count
+    #@page_count
+  #end
 
   #attr_accessor :genre
 
-  def genre=(genre)
-    @genre=genre
-  end
+  #def genre=(genre)
+    #@genre=genre
+  #end
 
-  def genre
-    @genre
-  end
+  #def genre
+    #@genre
+  #end
 
   def turn_page
     puts "Flipping the page...wow, you read fast!"
